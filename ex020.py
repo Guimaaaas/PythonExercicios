2ad729d84d65d41d82dfae39ddecf085d9766d77
Artur Guimarães <51428160+Guimaaaas@users.noreply.github.com>
2024-05-27 17:17:27 -0300

    Create ex020.py
    sortear uma ordem de apresentação

---
+++ ex020.py
@@ -0,0 +1,10 @@
+import random
+aluno1 = input('Digite o nome do primeiro aluno: ')
+aluno2 = input('Digite o nome do segundo aluno: ')
+aluno3 = input('Digite o nome do terceiro aluno: ')
+aluno4 = input('Digite o nome do quarto aluno: ')
+alunos = [aluno1, aluno2, aluno3, aluno4]
+random.shuffle(alunos)
+
+print('A ordem de apresentação será: ')
+print(alunos)
