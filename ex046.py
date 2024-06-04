02b54ff42f7533af25f45d709c02304d21f244cc
Artur Guimarães <51428160+Guimaaaas@users.noreply.github.com>
2024-06-04 16:03:05 -0300

    Create ex046.py
    contagem regressiva para uma mensagem de natal

---
+++ ex046.py
@@ -0,0 +1,9 @@
+from time import sleep
+from emoji import emojize
+for c in range(10,0,-1):
+    print(c)
+    sleep(1)
+print(emojize('°❆⛄⋆.ೃ࿔🦌*:･❄️'*2))
+print(emojize('🎄 \033[31m༺𝓜𝓮𝓻𝓻𝔂༻༺𝓒𝓱𝓻𝓲𝓼𝓽𝓶𝓪𝓼༻\033[m🎄'))
+print(emojize('°❆⛄⋆.ೃ࿔🦌*:･❄️'*2))
+
